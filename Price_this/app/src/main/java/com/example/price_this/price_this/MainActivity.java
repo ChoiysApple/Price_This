d8b4a3f6e36b9f503ef7655f207b68863697d365
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-05-02 15:11:15 +0900

    Project commit
    Commit empty project

---
+++ Price_this/app/src/main/java/com/example/price_this/price_this/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.price_this.price_this;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
