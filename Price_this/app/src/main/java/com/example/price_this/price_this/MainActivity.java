2cb46f7249b30c3c8a523c94865de7972b29e68c
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-05-28 13:56:30 +0900

    password condition, alert

--- Price_this/app/src/main/java/com/example/price_this/price_this/MainActivity.java
+++ Price_this/app/src/main/java/com/example/price_this/price_this/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.price_this.price_this;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +10,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mUsernameField;
     private Button sign_up;
     private Button goto_sign_in;
+    private TextView account_guide;
     private static final String TAG = "EmailPassword";
 
     @Override
@@ -39,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         mEmailField = findViewById(R.id.field_email);
         mPasswordField = findViewById(R.id.field_password);
         mUsernameField = findViewById(R.id.field_username);
+        account_guide = findViewById(R.id.account_guide);
 
         sign_up = (Button) findViewById(R.id.btn_signup);
         sign_up.setOnClickListener(new View.OnClickListener() {
@@ -69,16 +76,43 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                            /*set username*/
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(mUsernameField.getText().toString())
+                                    .build();
+
+                            final String name = user.getDisplayName();
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d(TAG, name);
+                                            }
+                                        }
+                                    });
+
                             Toast.makeText(getApplicationContext(), "SIgn up succesful", Toast.LENGTH_LONG).show();
+                            Intent intent=new Intent(MainActivity.this,signIn.class);
+                            startActivity(intent);
 
                         } else {
                             // If sign in fails, display a message to the user.
+                            try {
+                                throw task.getException();
+                            } catch(FirebaseAuthWeakPasswordException e) {
+                                account_guide.setTextColor(Color.RED);
+                            } catch (Exception e) {
+                            }
+
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
                         }
-                        finish();
+
                         // [END_EXCLUDE]
                     }
                 });
